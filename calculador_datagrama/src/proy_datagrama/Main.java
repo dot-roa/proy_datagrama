e696108ef7cef51b81f52541a82802070a42203d
marodriguezn <marodriguezn@uqvirtual.com>
2020-09-02 18:42:16 -0500

    Lo logre prro, hice el metodo que imprime por ultima vez :v

--- calculador_datagrama/src/proy_datagrama/Main.java
+++ calculador_datagrama/src/proy_datagrama/Main.java
@@ -1,13 +1,18 @@
 package proy_datagrama;
 
 public class Main {
-	
-	public static void main(String[]args) {
+
+	public static void main(String[] args) {
 		mensajear_roa();
+		printMessage();
 	}
 
-public static void mensajear_roa() {
-	System.out.println("Hola mundo vamos a probar la magia de git :D");
-}
+	public static void mensajear_roa() {
+		System.out.println("Hola mundo vamos a probar la magia de git :D");
+	}
+
+	public static void printMessage() {
+		System.out.println("Ultima prueba prro");
+	}
 	
 }
