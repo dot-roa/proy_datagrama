15af7d31520bac420b1683eb04a03fc3a0c04254
Juan Manuel Roa Mejia <juanmanuelrm2009@gmail.com>
2020-09-01 20:10:22 -0500

    Agregue un metodo de mensaje hola mundo

--- proy_datagrama/src/proy_datagrama/Main.java
+++ proy_datagrama/src/proy_datagrama/Main.java
@@ -3,7 +3,11 @@ package proy_datagrama;
 public class Main {
 	
 	public static void main(String[]args) {
-		
+		mensajear_roa();
 	}
 
+public static void mensajear_roa() {
+	System.out.println("Hola mundo vamos a probar la magia de git :D");
+}
+	
 }
