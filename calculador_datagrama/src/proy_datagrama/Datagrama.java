38e6e3dcc13496c5cd7fc99fc64e043eec60a405
Juan Manuel Roa Mejia <juanmanuelrm2009@gmail.com>
2020-09-02 20:08:35 -0500

    Metodo de fragmentación por revisar

--- calculador_datagrama/src/proy_datagrama/Datagrama.java
+++ calculador_datagrama/src/proy_datagrama/Datagrama.java
@@ -1,5 +1,20 @@
 package proy_datagrama;
 
+import java.util.Scanner;
+
 public class Datagrama {
 
+	public void fragmentar() {
+		int tam_datagrama = 0;
+		int mtu = 0;
+		
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Por favor ingrese el");
+		tam_datagrama = Integer.parseInt(scn.nextLine());
+		mtu = Integer.parseInt(scn.nextLine());
+		
+		//TO-DO: Verificar que la fragmentación se haga de esta manera en las diapos.
+		int datagrama_headerless = tam_datagrama - 8;
+		int fragmentos = (int) Math.ceil(datagrama_headerless / mtu);
+	}
 }
