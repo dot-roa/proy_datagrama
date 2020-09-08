9b76f6cddf3c1cee1947d8bc5ac89e9233d43e9c
Wilzhar <wilmar2545@gmail.com>
2020-09-08 10:47:02 -0500

    Se agrego el dato del desplazamiento a la tabla

--- calculador_datagrama/src/proy_datagrama/Tabla.java
+++ calculador_datagrama/src/proy_datagrama/Tabla.java
@@ -3,20 +3,24 @@ package proy_datagrama;
 import java.util.ArrayList;
 
 public class Tabla {
-	ArrayList<String> filas = new ArrayList<String>();
+	private ArrayList<String> filas = new ArrayList<String>();
+	private Datagrama datagrama = new Datagrama();
 	
 	public Tabla() {
+		 
+		int[] desplazamientos = datagrama.calcularDesplazamiento(mtu, cantFragmentos);
 		
 		String fila = "";
+		construirFila(desplazamientos[i]);
 		
 		filas.add(fila);
 	}
 	
-	public String construirFila() {
+	public String construirFila(int desplazamiento) {
 		String fila = "";
-		
+		fila += desplazamiento + "\t";
+		fila += Util.convertirDecimalAStringBinario(desplazamiento);
 		return fila;
-		
 	}
 	
 }
