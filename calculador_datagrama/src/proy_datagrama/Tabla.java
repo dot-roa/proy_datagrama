9fd3a3d484a97a41ba10c228230a6c884b45e97d
marodriguezn <marodriguezn@uqvirtual.com>
2020-09-08 10:44:47 -0500

    Agregue un arraylist de nombre flags

--- calculador_datagrama/src/proy_datagrama/Tabla.java
+++ calculador_datagrama/src/proy_datagrama/Tabla.java
@@ -6,7 +6,7 @@ public class Tabla {
 	ArrayList<String> filas = new ArrayList<String>();
 	
 	public Tabla() {
-		
+		ArrayList<String> flags = new ArrayList<>();
 		String fila = "";
 		
 		filas.add(fila);
