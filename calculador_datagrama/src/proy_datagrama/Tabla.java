7601d1b2768f963df87b1139ed307548b5d993e2
Juan Manuel Roa Mejia <juanmanuelrm2009@gmail.com>
2020-09-08 10:20:17 -0500

    Ajustes de marco de trabajo

--- calculador_datagrama/src/proy_datagrama/Tabla.java
+++ calculador_datagrama/src/proy_datagrama/Tabla.java
@@ -6,6 +6,15 @@ public class Tabla {
 	ArrayList<String> filas = new ArrayList<String>();
 	
 	public Tabla() {
+		String fila = "";
+		
+		filas.add(fila);
+	}
+	
+	public String construirFila() {
+		String fila = "";
+		
+		return fila;
 		
 	}
 	
